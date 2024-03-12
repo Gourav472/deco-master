e7e65fb280c555785f1f022cc502aaa1855766d6
unknown <gouravbishnoi429@gmail.com>
2024-03-12 16:04:34 +0530

    first commit

---
+++ assects/js/main.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+}
+// preLoader//
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("load", function () {
+      setTimeout(function () {
+        let loadingOverlay = document.getElementById("loadingOverlay");
+        loadingOverlay.style.display = "none";
+        document.querySelector("body").classList.remove("overflow_hidden");
+      }, 3000);
+    });
+  });
