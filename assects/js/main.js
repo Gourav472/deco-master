c3df7164c7ec3741aa70a2bbe2197174481df031
unknown <gouravbishnoi429@gmail.com>
2024-03-13 09:10:16 +0530

    pre-loader added

--- assects/js/main.js
+++ assects/js/main.js
@@ -1,15 +1,16 @@
+
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
 }
 // preLoader//
 document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("load", function () {
-      setTimeout(function () {
-        let loadingOverlay = document.getElementById("loadingOverlay");
-        loadingOverlay.style.display = "none";
-        document.querySelector("body").classList.remove("overflow_hidden");
-      }, 3000);
-    });
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
   });
+});
